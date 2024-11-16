ae3849f30aca39ca34993deee1447224e7e954c8
Murad501 <murad.hossain5501@gmail.com>
2024-11-16 23:06:06 +0600

    day 7 added

---
+++ Semester 3/Weeks/Week 3/Day-7/C_Premutation.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define nl '\n'
+#define ll long long int
+
+#define MuRAD_BOOST()                 \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+
+int main()
+{
+    MuRAD_BOOST();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        // map<int, int> mp;
+        // int arr[n][n-1];
+        // vector<int> v;
+        vector<map<int, int>> mp;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n - 1; j++)
+            {
+                int x;
+                cin >> x;
+                
+            }
+        }
+
+        // int j = 0;
+        // while (j < n - 1)
+        // {
+        //     int i = 0;
+        //     while (i < n)
+        //     {
+        //         int x = arr[i][j];
+        //         mp[x]++;
+        //         if (mp[x] == n - 1)
+        //         {
+        //             v.push_back(x);
+        //         }
+        //         i++;
+        //     }
+  
+        //     j++;
+        // }
+
+        // for (int i = 0; i < n; i++)
+        // {
+        //     cout << v[i] << " ";
+        // }
+        // cout << nl;
+    }
+    return 0;
+}
