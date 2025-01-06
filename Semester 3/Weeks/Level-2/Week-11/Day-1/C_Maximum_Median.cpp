3efda9621845eb0c2cc6f0fedc3b60443a5a029f
Murad501 <murad.hossain5501@gmail.com>
2025-01-06 22:46:53 +0600

    day 1 added

---
+++ Semester 3/Weeks/Level-2/Week-11/Day-1/C_Maximum_Median.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define nl '\n'
+#define all(x) x.begin(), x.end()
+#define ll long long int
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define cyes cout << "Yes" << '\n'
+#define cno cout << "No" << '\n'
+#define minus cout << -1 << '\n'
+#define zero cout << 0 << '\n'
+
+#define MuRAD_BOOST()                 \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<int> v;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        v.push_back(x);
+    }
+
+    auto ok = [&](ll median)
+    {
+        ll cnt = 0;
+        for (int i = n / 2; i < n; i++)
+        {
+            cnt += (v[i] < median ? (median - v[i]) : 0);
+        }
+        return cnt <= k;
+    };
+
+    sort(v.begin(), v.end());
+    ll l = 1, r = 2e9, mid, ans;
+    while (l <= r)
+    {
+        mid = l + (r - l) / 2;
+        if (ok(mid))
+        {
+            ans = mid;
+            l = mid + 1;
+        }
+        else
+        {
+            r = mid - 1;
+        }
+    }
+
+    cout << ans << nl;
+}
+
+int main()
+{
+    MuRAD_BOOST();
+    solve();
+
+    return 0;
+}
