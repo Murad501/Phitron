4c50a3d8de94a5197b33c2dfa7ef30bd209c626f
Murad501 <murad.hossain5501@gmail.com>
2025-01-22 23:00:22 +0600

    code updated

---
+++ Contests/CodeChef/Starters 170/Crazy_Jumping_Frogs.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define nl '\n'
+#define all(x) x.begin(), x.end()
+#define ll long long int
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define cyes cout << "Yes" << '\n'
+#define cno cout << "No" << '\n'
+#define minus cout << -1 << '\n'
+#define zero cout << 0 << '\n'
+
+#define MuRAD_BOOST()                 \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+
+void solve()
+{
+    int n;
+    cin>>n;
+    vector<int> v;
+    for(int i=0;i<n;i++)
+    {
+        int x;
+        cin>>x;
+        v.push_back(x);
+    }
+    sort(all(v));
+    for (int i = 0; i < n; i++)
+    {
+         cout<<v[i]<<" ";
+    }
+    cout<<nl;
+}
+
+int main()
+{
+    MuRAD_BOOST();
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
